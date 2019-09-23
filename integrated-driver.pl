1b69bc745c59529df4152e41e142dc5a435af67e
root <root@localhost.localdomain>
2019-09-23 12:29:14 -0700

    perltidy

--- integrated-driver.pl
+++ integrated-driver.pl
@@ -7,215 +7,218 @@ use strict;
 use warnings;
 use Device::SerialPort qw( :PARAM :STAT 0.07 );
 my $maker = shift;
-my $type = shift;
+my $type  = shift;
 
 my $gantry = init_gantry();
 
+my $ft = init_ft();
 
-  
-my $ft = init_ft(); 
+my $step             = 0.01;
+my $samples_per_step = 10;
+my $runs             = 3;
+my $force_max        = 180;
+open(
+    my $outfile,
+    ">",
+    "tester-$maker-$type-step-$step-mm-"
+      . $samples_per_step
+      . "-samples-averaged-per-step-"
+      . time() . ".csv"
+);
 
-my $step = 0.01;
-my $samples_per_step=10;
-my $runs = 3;
-my $force_max=180;
-open (my $outfile, ">", "tester-$maker-$type-step-$step-mm-".$samples_per_step."-samples-averaged-per-step-".time().".csv");
+for ( my $run = 1 ; $run <= $runs ; $run++ ) {
 
+    while (1) {
 
-for(my $run = 1; $run <= $runs; $run++) {
+        run_gantry_cmd( $gantry, "G1 Z-0.01" );
 
-while (1) {
+        my $result = average_n_measurements(1);
 
-run_gantry_cmd($gantry, "G1 Z-0.01");
+        if ( $result > 1 ) {
+            warn "We're good to go: We've homed to the top of the switch";
+            sleep(1);
+            last;
+        }
+    }
 
-my $result = average_n_measurements(1);
+    run_gantry_cmd( $gantry, "G1 Z1" );
 
-if ($result > 1) {
-	warn "We're good to go: We've homed to the top of the switch";
-	sleep(1);
-	last;
-}
-}
+    # Zero the force tester
+    run_force_tester_cmd( $ft, 0xaa, 0x01, 0x55 );
 
-run_gantry_cmd($gantry, "G1 Z1");
-# Zero the force tester
-run_force_tester_cmd($ft,0xaa,0x01,0x55);
+    # Throw away 10 measurements before we start off.
+    my $result = average_n_measurements(10);
 
-# Throw away 10 measurements before we start off.
-my $result = average_n_measurements(10);
+    my @data;
+    my $location = -1;
+    warn "# Downstroke\n";
+    while (1) {
 
-my @data;
-my $location = -1;
-warn "# Downstroke\n";
-while (1) {
+        run_gantry_cmd( $gantry, "G1 Z-" . $step );
 
-run_gantry_cmd($gantry, "G1 Z-".$step);
+        my $result = average_n_measurements($samples_per_step);
+        push @data, [ $run, 'downstroke', $location, $result ];
+        warn "Run $run - Downstroke - $location mm: $result g\n";
+        $location += $step;
+        if ( $result > $force_max ) {
+            warn "# Bottomed out after detecting $force_max g of force";
+            last;
+        }
 
-my $result = average_n_measurements($samples_per_step);
-push @data, [$run,'downstroke',$location, $result];
-warn "Run $run - Downstroke - $location mm: $result g\n";
-$location+= $step;
-if ($result > $force_max) {
-	warn "# Bottomed out after detecting $force_max g of force";
-	last;
-}
+    }
 
-}
+    warn "Upstroke\n";
+    while (1) {
 
-warn "Upstroke\n";
-while (1) {
+        run_gantry_cmd( $gantry, "G1 Z" . $step );
 
-run_gantry_cmd($gantry, "G1 Z".$step);
+        my $result = average_n_measurements($samples_per_step);
+        warn "Run - $run - Upstroke - $location mm: $result g\n";
+        push @data, [ $run, 'upstroke', $location, $result ];
+        $location -= $step;
+        if ( $result <= 0 && $location < -1 ) {
+            warn "# All done!\n";
+            last;
+        }
 
-my $result = average_n_measurements($samples_per_step);
-warn "Run - $run - Upstroke - $location mm: $result g\n";
-push @data, [$run,'upstroke',$location, $result];
-$location-= $step;
-if ($result <=0 && $location < -1 ) {
-	warn "# All done!\n";
-	last;
-}
-
-}
-for my $row (@data) {
-print $outfile join(",",@$row)."\n";
-}
+    }
+    for my $row (@data) {
+        print $outfile join( ",", @$row ) . "\n";
+    }
 
 }
 exit;
 
-
 sub bail_out {
-run_gantry_cmd($gantry, "G1 Z10");
-die "We had something crazy happen. bailed.";
+    run_gantry_cmd( $gantry, "G1 Z10" );
+    die "We had something crazy happen. bailed.";
 }
 
 sub average_n_measurements {
-	my $samples = shift;
-my $result = 0;
-for(my $i = 0; $i<$samples;$i++) {
-my $point = get_next_force_measurement();
-$result += $point;
-}
-$result = $result/$samples; # 
-return $result;
+    my $samples = shift;
+    my $result  = 0;
+    for ( my $i = 0 ; $i < $samples ; $i++ ) {
+        my $point = get_next_force_measurement();
+        $result += $point;
+    }
+    $result = $result / $samples;    #
+    return $result;
 }
 
 my @current_force_measurement;
 
 sub get_next_force_measurement {
 
-while(1){
-    if (my $byte = $ft->read(1)) {
-	if (ord($byte) ==  0xAA && ($#current_force_measurement >=5)) {  # 170
-		#		previous packet done;	
-		my @result = @current_force_measurement;
-		@current_force_measurement =  ($byte);
-		if ($#result == 6) {
-	 	return	return_measurement(@result);
-		}
-	} else {
-		push @current_force_measurement, $byte;
-	}
-		
+    while (1) {
+        if ( my $byte = $ft->read(1) ) {
+            if ( ord($byte) == 0xAA && ( $#current_force_measurement >= 5 ) )
+            {                        # 170
+                                     #		previous packet done;
+                my @result = @current_force_measurement;
+                @current_force_measurement = ($byte);
+                if ( $#result == 6 ) {
+                    return return_measurement(@result);
+                }
+            }
+            else {
+                push @current_force_measurement, $byte;
+            }
+
+        }
     }
 }
-}
-
-
-
 
 #run_gantry_cmd("G1 Z".$offset);
 
-
-
-
-
 sub run_gantry_cmd {
-	my $gantry = shift;
-	my $output_string =shift;
-	my       $count_out = $gantry->write($output_string."\r");
-      warn "write failed\n"         unless ($count_out);
+    my $gantry        = shift;
+    my $output_string = shift;
+    my $count_out     = $gantry->write( $output_string . "\r" );
+    warn "write failed\n" unless ($count_out);
 }
-
-
-
 
 sub init_ft {
 
-my $force_tester_port = '/dev/serial/by-id/usb-Silicon_Labs_CP2102_USB_to_UART_Bridge_Controller_0001-if00-port0';
-my $ft = Device::SerialPort->new($force_tester_port, 1)
-    || die "Can't open $force_tester_port: $!";
+    my $force_tester_port =
+'/dev/serial/by-id/usb-Silicon_Labs_CP2102_USB_to_UART_Bridge_Controller_0001-if00-port0';
+    my $ft = Device::SerialPort->new( $force_tester_port, 1 )
+      || die "Can't open $force_tester_port: $!";
 
-my $data = $ft->databits(8);
-my $baud = $ft->baudrate(57600);
-my $parity = $ft->parity("none");
-$ft->buffers(7,7);
-$ft->stopbits(1);
-$ft->handshake('none');
-#$ft->handshake("rts");
+    my $data   = $ft->databits(8);
+    my $baud   = $ft->baudrate(57600);
+    my $parity = $ft->parity("none");
+    $ft->buffers( 7, 7 );
+    $ft->stopbits(1);
+    $ft->handshake('none');
 
+    #$ft->handshake("rts");
 
-$ft->write_settings or die "no settings\n";
+    $ft->write_settings or die "no settings\n";
 
+    # Set output to kg f
+    run_force_tester_cmd( $ft, 0xaa, 0x04, 0x55 );
 
-# Set output to kg f
-run_force_tester_cmd($ft,0xaa,0x04,0x55);
-# set output to realtime
-#run_force_tester_cmd($ft,0xaa,0x03,0x55);
-return $ft;
+    # set output to realtime
+    #run_force_tester_cmd($ft,0xaa,0x03,0x55);
+    return $ft;
 
 }
 
-sub run_force_tester_cmd{
-	my $ft = shift;
-	my @bytes = (@_);
-my $output_string = pack("C*", @bytes);
-my       $count_out = $ft->write($output_string);
-      warn "write failed\n"         unless ($count_out);
+sub run_force_tester_cmd {
+    my $ft            = shift;
+    my @bytes         = (@_);
+    my $output_string = pack( "C*", @bytes );
+    my $count_out     = $ft->write($output_string);
+    warn "write failed\n" unless ($count_out);
 }
 
 sub init_gantry {
 
+    my $gantry_port =
+      '/dev/serial/by-id/usb-Malyan_System_Malyan_3D_Printer_2060396E4752-if00';
+    my $gantry = Device::SerialPort->new( $gantry_port, 1 )
+      || die "Can't open $gantry_port: $!";
 
-my $gantry_port = '/dev/serial/by-id/usb-Malyan_System_Malyan_3D_Printer_2060396E4752-if00';
-my $gantry = Device::SerialPort->new($gantry_port, 1)
-    || die "Can't open $gantry_port: $!";
-    
+    my $data   = $gantry->databits(8);
+    my $baud   = $gantry->baudrate(19200);
+    my $parity = $gantry->parity("none");
+    $gantry->buffers( 8, 8 );
+    $gantry->stopbits(1);
+    $gantry->handshake('none');
 
-my $data = $gantry->databits(8);
-my $baud = $gantry->baudrate(19200);
-my $parity = $gantry->parity("none");
-$gantry->buffers(8,8);
-$gantry->stopbits(1);
-$gantry->handshake('none');
-#$gantry->handshake("rts");
+    #$gantry->handshake("rts");
 
-$gantry->write_settings or die "no settings\n";
-run_gantry_cmd($gantry,"G21") ; # set output to mm;
-run_gantry_cmd($gantry,"G91") ; # set motion to relative
+    $gantry->write_settings or die "no settings\n";
+    run_gantry_cmd( $gantry, "G21" );    # set output to mm;
+    run_gantry_cmd( $gantry, "G91" );    # set motion to relative
 
-return $gantry;
+    return $gantry;
 }
 
-
 sub return_measurement {
-	my @data = (@_);
-	if (ord($data[0]) == 0xAA &&  ord($data[6]) == 0x55) {
-		if (ord($data[5]) == 0x2C) {
-		} elsif (ord($data[5]) == 0x0C) {
-		} else { warn "neither pos nor neg: ".ord($data[5]);}
-		my $value = ord($data[4]) + (256 * ord($data[3])) + (256 * 256 * ord($data[2])) + (256 * 256 * 256 * ord($data[1]));
-		if (ord($data[5]) == 0x0C) {
-			$value = 0-$value;
-		}
-		#warn "Force measured: ".$value."\n";
-		if ($value > 250)	{
-			die "Got a bad measurement. Bailing out. We got $value\n";
-		}
-		return $value;
-	}	
-	else {
-		die "had bad data in our measurement";
-	}
+    my @data = (@_);
+    if ( ord( $data[0] ) == 0xAA && ord( $data[6] ) == 0x55 ) {
+        if ( ord( $data[5] ) == 0x2C ) {
+        }
+        elsif ( ord( $data[5] ) == 0x0C ) {
+        }
+        else { warn "neither pos nor neg: " . ord( $data[5] ); }
+        my $value =
+          ord( $data[4] ) +
+          ( 256 * ord( $data[3] ) ) +
+          ( 256 * 256 * ord( $data[2] ) ) +
+          ( 256 * 256 * 256 * ord( $data[1] ) );
+        if ( ord( $data[5] ) == 0x0C ) {
+            $value = 0 - $value;
+        }
+
+        #warn "Force measured: ".$value."\n";
+        if ( $value > 250 ) {
+            die "Got a bad measurement. Bailing out. We got $value\n";
+        }
+        return $value;
+    }
+    else {
+        die "had bad data in our measurement";
+    }
 }
